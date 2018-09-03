a5f4478bac4607bd69dc0e7266e6f1997ba30d09
brendadev <brenda@koneksahealth.com>
2018-09-02 23:13:50 -0400

    added inventory db helper

---
+++ app/src/main/java/com/example/android/storeinventoryapp/data/InventoryDbHelper.java
@@ -0,0 +1,41 @@
+package com.example.android.storeinventoryapp.data;
+import com.example.android.storeinventoryapp.data.InventoryContract.InventoryEntry;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class InventoryDbHelper extends SQLiteOpenHelper {
+    public final static int DATABASE_VERSION = 1;
+    public final static String DATABASE_NAME = "inventory.db";
+
+    private final static String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + InventoryEntry.TABLE_NAME + " (" +
+            InventoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            InventoryEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, " +
+            InventoryEntry.COLUMN_PRODUCT_PRICE + " FLOAT NOT NULL, " +
+            InventoryEntry.COLUMN_PRODUCT_QUANTITY + " INTEGER NOT NULL DEFAULT 0, " +
+            InventoryEntry.COLUMN_PRODUCT_SUPPLIER + " TEXT, " +
+            InventoryEntry.COLUMN_SUPPLIER_PHONE + " TEXT, " +
+            InventoryEntry.COLUMN_PRODUCT_DESCRIPTION + " TEXT );";
+
+    private final static String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + InventoryEntry.TABLE_NAME;
+
+    public InventoryDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
