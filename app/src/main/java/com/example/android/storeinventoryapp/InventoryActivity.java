eb2cb9b83270b6aa5c6d405dcf22c903bd3f3c8d
brendadev <brenda@koneksahealth.com>
2018-09-03 23:06:44 -0400

    added options menu

--- app/src/main/java/com/example/android/storeinventoryapp/InventoryActivity.java
+++ app/src/main/java/com/example/android/storeinventoryapp/InventoryActivity.java
@@ -2,6 +2,7 @@ package com.example.android.storeinventoryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class InventoryActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class InventoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_inventory, menu);
+        return true;
     }
 }
