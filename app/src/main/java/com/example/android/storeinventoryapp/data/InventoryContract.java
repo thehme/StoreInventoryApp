1068f545e659d2171d4a1fe49a48063f45f6d0ce
brendadev <brenda@koneksahealth.com>
2018-09-13 18:38:53 -0400

    added content list and item ypes

--- app/src/main/java/com/example/android/storeinventoryapp/data/InventoryContract.java
+++ app/src/main/java/com/example/android/storeinventoryapp/data/InventoryContract.java
@@ -1,5 +1,6 @@
 package com.example.android.storeinventoryapp.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -7,6 +8,11 @@ public final class InventoryContract {
     public static final String CONTENT_AUTHORITY = "com.example.android.storeinventoryapp";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
     public static final String PATH_INVENTORY = "inventory";
+
+    public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE +
+            "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+    public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE +
+            "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
 
     private InventoryContract() {
     }
