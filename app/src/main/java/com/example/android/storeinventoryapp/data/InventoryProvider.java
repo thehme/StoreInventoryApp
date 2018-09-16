088bd29e7c811476b53ffa1b52baf7234ad2632e
brendadev <brenda@koneksahealth.com>
2018-09-16 11:17:22 -0400

    added manifest declaration for content provider, provider file, and change direct query to db to use content resolver and content provider

---
+++ app/src/main/java/com/example/android/storeinventoryapp/data/InventoryProvider.java
@@ -0,0 +1,100 @@
+package com.example.android.storeinventoryapp.data;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.example.android.storeinventoryapp.data.InventoryContract.InventoryEntry;
+
+public class InventoryProvider extends ContentProvider {
+    public static final String TAG = InventoryProvider.class.getSimpleName();
+    private InventoryDbHelper inventoryDbHelper;
+
+    // uri codes
+    private static final int BOOKS = 100;
+    private static final int BOOK_ID = 101;
+
+    // define uri matcher
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_INVENTORY, BOOKS);
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_INVENTORY + "/#", BOOK_ID);
+    }
+
+    /**
+     * To initialize the content provider and db helper object
+     * @return true
+     */
+    @Override
+    public boolean onCreate() {
+        // initialize db helper object to gain access to pets db
+        inventoryDbHelper = new InventoryDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection,
+                        @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        // Get readable db
+        SQLiteDatabase database = inventoryDbHelper.getReadableDatabase();
+
+        // define cursor to hold result from query operation
+        Cursor cursor;
+
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case BOOKS:
+                // query directly with the parameters provider
+                // cursor will be returned with all data
+                cursor = database.query(InventoryEntry.TABLE_NAME, projection,
+                        selection, selectionArgs, null, null, sortOrder);
+                break;
+            case BOOK_ID:
+                // setup query string
+                selection = InventoryEntry._ID + "=?";
+                // get uri id to use in query string
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+
+                // perform query on db for single item
+                cursor = database.query(InventoryEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        // set listener up for change in db
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}
