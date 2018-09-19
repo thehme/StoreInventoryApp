35cf8ecaef75e3d9dffc1c47631b5982a1fe64fb
brendadev <brenda@koneksahealth.com>
2018-09-19 18:21:50 -0400

    added logic to open editor activity when floating button is clicked

--- app/src/main/java/com/example/android/storeinventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/android/storeinventoryapp/EditorActivity.java
@@ -1,6 +1,13 @@
 package com.example.android.storeinventoryapp;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class EditorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
 }
