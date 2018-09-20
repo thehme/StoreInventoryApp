b311f81254e87826dc905daea28d4463b1fddf6a
brendadev <brenda@koneksahealth.com>
2018-09-19 23:12:00 -0400

    change activity title depending on whether uri exists

--- app/src/main/java/com/example/android/storeinventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/android/storeinventoryapp/EditorActivity.java
@@ -1,13 +1,26 @@
 package com.example.android.storeinventoryapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class EditorActivity extends AppCompatActivity {
+    private Uri mCurrentPetUri;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        Intent intent = getIntent();
+        mCurrentPetUri = intent.getData();
+
+        if (mCurrentPetUri != null) {
+            setTitle(R.string.edit_book);
+        } else {
+            setTitle(R.string.add_book);
+        }
     }
 }
