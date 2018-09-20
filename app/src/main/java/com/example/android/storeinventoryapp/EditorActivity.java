2a4003a32e4f49936d6eb3630dfe940283cacd51
brendadev <brenda@koneksahealth.com>
2018-09-20 08:25:32 -0400

    added options menu to editor view which I will use to save the entry form the user

--- app/src/main/java/com/example/android/storeinventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/android/storeinventoryapp/EditorActivity.java
@@ -3,10 +3,16 @@ package com.example.android.storeinventoryapp;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class EditorActivity extends AppCompatActivity {
+public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks {
     private Uri mCurrentPetUri;
 
     @Override
@@ -22,5 +28,39 @@ public class EditorActivity extends AppCompatActivity {
         } else {
             setTitle(R.string.add_book);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // inflate the options menu from the /res/menu/menu_editor.xml file
+        // to add menu item in the nav bar
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        if (mCurrentPetUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
+        return true;
+    }
+
+    @NonNull
+    @Override
+    public Loader onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader loader, Object data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader loader) {
+
     }
 }
